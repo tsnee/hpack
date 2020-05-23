dbde00d813ea330681ab91e1fdeda51e5e1778a7
Tom Snee <351700+tsnee@users.noreply.github.com>
2020-05-23 13:22:13 -0400

    Refactor benchmark code to satisfy JMH.

--- src/main/scala/io/github/tsnee/hpack/HpackVectorBenchmark.scala
+++ src/main/scala/io/github/tsnee/hpack/HpackVectorBenchmark.scala
@@ -6,14 +6,15 @@ import zio.Chunk
 
 class HpackVectorBenchmark extends HpackBenchmark {
   implicit def forConvenience(i: Int): Byte = i.toByte
-  val emptyCtx = VectorDecoderContext(DynamicTable(1024))
 
   @Benchmark
-  override def decodingAnEmptyHeaderBlockYieldsAnEmptyHeaderList =
+  override def decodingAnEmptyHeaderBlockYieldsAnEmptyHeaderList = {
+    val emptyCtx = new ChunkDecoderContext(DynamicTable(1024))
     VectorDecoder
       .decode(Chunk.empty, emptyCtx)
       .getOrElse(emptyCtx)
       .headerList
+  }
 
   @Benchmark
   override def rfc7541AppendixC_1_1 = {
@@ -38,6 +39,7 @@ class HpackVectorBenchmark extends HpackBenchmark {
 
   @Benchmark
   override def rfc7541AppendixC_2_1_in_one_chunk = {
+    val emptyCtx = new ChunkDecoderContext(DynamicTable(1024))
     val chunk: Chunk[Byte] = Chunk(
       0x40, 0x0A, 0x63, 0x75, 0x73, 0x74, 0x6F, 0x6D, 0x2D, 0x6B, 0x65,
       0x79, 0x0D, 0x63, 0x75, 0x73, 0x74, 0x6F, 0x6D, 0x2D, 0x68, 0x65,
@@ -51,6 +53,7 @@ class HpackVectorBenchmark extends HpackBenchmark {
 
   @Benchmark
   override def rfc7541AppendixC_2_1_in_two_chunks = {
+    val emptyCtx = new ChunkDecoderContext(DynamicTable(1024))
     val first: Chunk[Byte] = Chunk(
       0x40, 0x0A, 0x63, 0x75, 0x73, 0x74, 0x6F, 0x6D, 0x2D, 0x6B, 0x65
     )
@@ -69,6 +72,7 @@ class HpackVectorBenchmark extends HpackBenchmark {
 
   @Benchmark
   override def rfc7541AppendixC_2_2 = {
+    val emptyCtx = new ChunkDecoderContext(DynamicTable(1024))
     val chunk: Chunk[Byte] = Chunk(
       0x04, 0x0C, 0x2F, 0x73, 0x61, 0x6D, 0x70, 0x6C, 0x65, 0x2F, 0x70,
       0x61, 0x74, 0x68
@@ -81,6 +85,7 @@ class HpackVectorBenchmark extends HpackBenchmark {
 
   @Benchmark
   override def rfc7541AppendixC_2_3 = {
+    val emptyCtx = new ChunkDecoderContext(DynamicTable(1024))
     val chunk: Chunk[Byte] = Chunk(
       0x10, 0x08, 0x70, 0x61, 0x73, 0x73, 0x77, 0x6F, 0x72, 0x64, 0x06,
       0x73, 0x65, 0x63, 0x72, 0x65, 0x74
@@ -93,6 +98,7 @@ class HpackVectorBenchmark extends HpackBenchmark {
 
   @Benchmark
   override def rfc7541AppendixC_2_4 = {
+    val emptyCtx = new ChunkDecoderContext(DynamicTable(1024))
     val chunk: Chunk[Byte] = Chunk.single(0x82)
     VectorDecoder
       .decode(chunk, VectorDecoderContext(DynamicTable(1024)))
@@ -102,6 +108,7 @@ class HpackVectorBenchmark extends HpackBenchmark {
 
   @Benchmark
   override def rfc7541AppendixC_3_1 = {
+    val emptyCtx = new ChunkDecoderContext(DynamicTable(1024))
     val chunk: Chunk[Byte] = Chunk(
       0x82, 0x86, 0x84, 0x41, 0x0F, 0x77, 0x77, 0x77, 0x2E, 0x65, 0x78,
       0x61, 0x6D, 0x70, 0x6C, 0x65, 0x2E, 0x63, 0x6F, 0x6D
@@ -114,6 +121,7 @@ class HpackVectorBenchmark extends HpackBenchmark {
 
   @Benchmark
   override def rfc7541AppendixC_3_2 = {
+    val emptyCtx = new ChunkDecoderContext(DynamicTable(1024))
     val first: Chunk[Byte] = Chunk(
       0x82, 0x86, 0x84, 0x41, 0x0F, 0x77, 0x77, 0x77, 0x2E, 0x65, 0x78,
       0x61, 0x6D, 0x70, 0x6C, 0x65, 0x2E, 0x63, 0x6F, 0x6D
@@ -135,6 +143,7 @@ class HpackVectorBenchmark extends HpackBenchmark {
 
   @Benchmark
   override def rfc7541AppendixC_3_3 = {
+    val emptyCtx = new ChunkDecoderContext(DynamicTable(1024))
     val first: Chunk[Byte] = Chunk(
       0x82, 0x86, 0x84, 0x41, 0x0F, 0x77, 0x77, 0x77, 0x2E, 0x65, 0x78,
       0x61, 0x6D, 0x70, 0x6C, 0x65, 0x2E, 0x63, 0x6F, 0x6D
@@ -165,6 +174,7 @@ class HpackVectorBenchmark extends HpackBenchmark {
   }
 
   override def rfc7541AppendixC_4_1 = {
+    val emptyCtx = new ChunkDecoderContext(DynamicTable(1024))
     val first: Chunk[Byte] = Chunk(
       0x82, 0x86, 0x84, 0x41, 0x8C, 0xF1, 0xE3, 0xC2, 0xE5, 0xF2, 0x3A,
       0x6B, 0xA0, 0xAB, 0x90, 0xF4, 0xFF
